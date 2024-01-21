1d57231f92e8b9697da93d349737f19b006dc96d
Mukul Nayak <mukulnayak2004@gmail.com>
2024-01-21 15:16:37 +0530

    Updated

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,7 +3,7 @@ import Footer from "./components/Footer";
 import HeartData from "./components/HeartData";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
-import DataForm from "./components/dataForm";
+import DataForm from "./components/DataForm";
 import { BrowserRouter,Routes,Route } from "react-router-dom";
 function App() {
 
